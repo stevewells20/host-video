c6fc5a88965bda9265f4d60fcccf473ab5649d59
Steven Wells <stevewells20@gmail.com>
2018-06-17 12:35:48 -0700

    stuf

--- OOP/main.cpp
+++ OOP/main.cpp
@@ -23,25 +23,26 @@ int main(int argc, char **argv) {
 	//cout << getBuildInformation() << endl << endl;
 	GstStreamCapper gstr(portNum,true);
 	gstr.showLive = false;
-	gstr.sourceType = UDP; //UDP, CAM, VID
-	gstr.codec = H264;
+	gstr.sourceType = CAM; //UDP, CAM, VID
+	// gstr.codec = H264;
 	// gstr.videoFileLocation = "/usr/share/OpenCV/samples/data/data/vtest.avi";
 	// gstr.codecType = NONE;
 	// gstr.resetCap();
 
 	// GstHoughCircle fCircle;
 	// GstFaceDetect fFace;
-	GstEdgeDetect fEdge;//	fEdge.apply_contuors = false; fEdge.apply_hull = false; fEdge.apply_lines = false;
+	GstEdgeDetect fEdge;
+	fEdge.apply_contuors = false; fEdge.apply_hull = true; fEdge.apply_lines = false;
 	// GstColorCorrect2 fColor;
 	// GstLaplaceEdgeDetect fLapEdge;
 	// GstBRISKFeatures fBRISK;
 	// GstBRISKDetect fBRISK;
-	// GstTest fTest;
+	//  GstTest fTest;
 	// GstMotionDetect fMotion;
 
 	// gstr.addFilter(&fMotion);
 	// gstr.addFilter(&fCircle);
-	// gstr.addFilter(&fEdge);
+	gstr.addFilter(&fEdge);
 	// gstr.addFilter(&fFace);
 	// gstr.addFilter(&fColor);
 	// gstr.addFilter(&fLapEdge);
